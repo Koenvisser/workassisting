40d728d48d3bad0341764ae344e50cc6479aa62a
Visser,K.J.A. (Koen) <57984909+Koenvisser@users.noreply.github.com>
2025-04-09 22:30:20 +0200

    try to improve multi atomics

--- src/schedulers.rs
+++ src/schedulers.rs
@@ -1,3 +1,4 @@
 pub mod multi_atomics;
+pub mod multi_atomics_2;
 pub mod workassisting;
 pub mod workassisting_loop;
--- src/schedulers/workassisting_loop.rs
+++ src/schedulers/workassisting_loop.rs
@@ -1,21 +1,14 @@
 #[macro_export]
 macro_rules! for_each_scheduler {
   ($body: ident $(, $arg: expr)*) => {
-    $body::<crate::schedulers::multi_atomics::worker::Scheduler<64, 1, 1>>($($arg),*);
-    $body::<crate::schedulers::multi_atomics::worker::Scheduler<64, 10, 1>>($($arg),*);
     $body::<crate::schedulers::multi_atomics::worker::Scheduler<64, 1, 4>>($($arg),*);
-    $body::<crate::schedulers::workassisting::worker::Scheduler<1>>($($arg),*);
-    $body::<crate::schedulers::workassisting::worker::Scheduler<4>>($($arg),*);
   };
 }
 
 #[macro_export]
 macro_rules! for_each_scheduler_with_arg {
   ($body: ident, $arg1: expr $(, $arg: expr)*) => {
-    $arg1 = $body::<crate::schedulers::multi_atomics::worker::Scheduler<64, 1, 1>>($arg1 $(, $arg)*);
-    $arg1 = $body::<crate::schedulers::multi_atomics::worker::Scheduler<64, 10, 1>>($arg1 $(, $arg)*);
     $arg1 = $body::<crate::schedulers::multi_atomics::worker::Scheduler<64, 1, 4>>($arg1 $(, $arg)*);
-    $arg1 = $body::<crate::schedulers::workassisting::worker::Scheduler<1>>($arg1 $(, $arg)*);
-    $arg1 = $body::<crate::schedulers::workassisting::worker::Scheduler<4>>($arg1 $(, $arg)*);
+    $arg1 = $body::<crate::schedulers::multi_atomics_2::worker::Scheduler<64, 1, 4>>($arg1 $(, $arg)*);
   };
 }
